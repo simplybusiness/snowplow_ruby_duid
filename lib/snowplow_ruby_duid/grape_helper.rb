da4e9ccaa6a90c29eaedd9fb7cd085aa83522060
Michael Campbell (AAs) <michael.campbell@simplybusiness.com>
2025-07-28 16:59:34 -0400

    Grape based "helper" method + test
    NORMAL rack `requests.cookies` is a hash, Grape's is a
    `Grape::Cookies` type, which doesn't have the same API as a Ruby
    Hash. As such, this library uses the methods that `Grape::Cookies`
    actually emits/exposes/exports.
    
    Since this code is used as a Mixin, it requires the code it's mixed
    into contains `request`, `request.cookies`, etc. in the same way that
    this code requires it, so we rely on the caller to use the right one.
    
    The actual API "surface" is the same as the SnowplowRubyDuid::Helper.
    By design, no attempt to "share code" with the original
    SnowplowRubyDuid::Helper has been made.

---
+++ lib/snowplow_ruby_duid/grape_helper.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module SnowplowRubyDuid
+  # This is the same as SnowplowRubyDuid::Helper, except it handles the HTTP
+  # objects that the Grape API (https://github.com/ruby-grape/grape) exposes
+  # (`response`, `request`, `request.cookies`) in an appropriate manner.
+  module GrapeHelper
+    def snowplow_domain_userid
+      @snowplow_domain_userid ||= find_or_create_snowplow_domain_userid
+    end
+
+    private
+
+    def find_or_create_snowplow_domain_userid
+      find_snowplow_domain_userid || create_snowplow_domain_userid
+    end
+
+    def create_snowplow_domain_userid
+      request_created_at = Time.now
+      domain_userid      = DomainUserid.new.to_s
+      options = {
+        secure: Configuration.secure,
+        same_site: Configuration.same_site
+      }
+      snowplow_cookie = Cookie.new request.host, domain_userid, request_created_at, options
+
+      # Grape has no "response" object but uses the `header` DSL or the `cookies` local var
+      # header 'Set-Cookie', "#{snowplow_cookie.key}=#{snowplow_cookie.value}; Path=/; HttpOnly"
+      cookies[snowplow_cookie.key] = snowplow_cookie.value
+      domain_userid
+    end
+
+    # See: https://docs.snowplow.io/docs/sources/trackers/ruby-tracker/adding-data-events/#cookie-based-user-properties
+    def find_snowplow_domain_userid
+      snowplow_cookie = find_snowplow_cookie # [key, value] format
+      # The cookie value format: domainUserId.createTs.visitCount.nowTs.lastVisitTs
+      snowplow_cookie.last.split('.').first unless snowplow_cookie.nil?
+    end
+
+    # @uses request[Grape::Cookies]  Not a real parameter, but comes from the code this is mixed into.
+    # @return [Hash{string => string}, nil] Snowplow cookie key/value or nil if not found.
+    def find_snowplow_cookie
+      snowplow_cookie = nil
+      request.cookies.each do |(key, value)|
+        if key =~ /^#{KEY_PREFIX}/
+          snowplow_cookie = [key, value]
+          break
+        end
+      end
+      snowplow_cookie
+    end
+  end
+end
